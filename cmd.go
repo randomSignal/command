684b29730df6fde351ff7499bcad41a1376078ae
刘昌喜 <liuchangxi@querycap.com>
2020-01-19 21:17:11 +0800

    commit

---
+++ cmd.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"os"
+)
+
+type cmdFunc func() error
+
+type command struct {
+	funcMap map[string]cmdFunc
+}
+
+func NewCommand() *command {
+	return &command{
+		funcMap: make(map[string]cmdFunc),
+	}
+}
+
+func (cmd *command) Register(cmdString string, cmdFunc cmdFunc) {
+	// check repeat
+	if _, ok := cmd.funcMap[cmdString]; ok {
+		panic("register repeat")
+	}
+	cmd.funcMap[cmdString] = cmdFunc
+}
+
+func (cmd *command) Run() error {
+	if len(os.Args) != 2 {
+		return nil
+	}
+
+	cmdString := os.Args[1]
+	// check registered
+	if _, ok := cmd.funcMap[cmdString]; !ok {
+		panic("not register")
+	}
+
+	return cmd.funcMap[cmdString]()
+}
